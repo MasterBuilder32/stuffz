7afaa1e028330cfe08de0ce6053680238b27d11b
MasterBuilder32 <84044177+MasterBuilder32@users.noreply.github.com>
2021-07-31 10:04:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+Webcam.set({
+    width:305,
+    height:280,
+    image_format:'png',
+    png_quality:90,
+    constraints:{
+        facingMode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
